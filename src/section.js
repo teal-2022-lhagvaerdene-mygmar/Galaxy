d977159420422032ab699fb72d77d4f77d836877
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2023-01-02 17:11:46 +0800

    01-02

---
+++ src/section.js
@@ -0,0 +1,24 @@
+import './section.css'
+function Section(){
+    return(
+        <section>
+            <div className='card-d'>
+                <div>
+                    <img src="jeremy-thomas-4dpAqfTbvKA-unsplash.jpg" alt="qwertyuio" width={900}/>
+                </div>
+                <div >
+                    <p>Galaxies are sprawling systems of dust, gas, dark matter, and anywhere from a million to a trillion stars that are held together by gravity. Nearly all large galaxies are thought to also contain supermassive black holes at their centers. In our own galaxy, the Milky Way, the sun is just one of about 100 to 400 billion stars that spin around Sagittarius A*, a supermassive black hole that contains as much mass as four million suns.
+                    Before the 20th century, we didn't know that galaxies other than the Milky Way existed; earlier astronomers had classified them as as “nebulae,” since they looked like fuzzy clouds. But in the 1920s, astronomer Edwin Hubble showed that the Andromeda “nebula” was a galaxy in its own right. Since it is so far from us, it takes light from Andromeda more than 2.5 million years to bridge the gap. Despite the immense distance, Andromeda is the closest large galaxy to our Milky Way, and it's bright enough in the night sky that it's visible to the naked eye in the Northern Hemisphere.
+
+In 1936, Hubble debuted a way to classify galaxies, grouping them into four main types: spiral galaxies, lenticular galaxies, elliptical galaxies, and irregular galaxies.
+
+More than two-thirds of all observed galaxies are spiral galaxies. A spiral galaxy has a flat, spinning disk with a central bulge surrounded by spiral arms. That spinning motion, at speeds of hundreds of kilometers a second, may cause matter in the disk to take on a distinctive spiral shape, like a cosmic pinwheel. Our Milky Way, like other spiral galaxies, has a linear, starry bar at its center.
+
+Elliptical galaxies are shaped as their name suggests: They are generally round but can stretch longer along one axis than along the other, so much so that some take on a cigar-like appearance. The universe's largest-known galaxies—giant elliptical galaxies—can contain up to a trillion stars and span two million light-years across. Elliptical galaxies may also be small, in which case they are called dwarf elliptical galaxies.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Section
