a50822900deced677f27cd49e8d959894ba92275
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2023-02-10 17:08:00 +0800

    02-10

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,28 +1,38 @@
 import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, Route, Routes } from "react-router-dom";
 
 export function Home() {
   return (
     <>
       <HomeNavbar />
+      <div style={{ maxWidth: 1100 }}>
+        <Routes>
+          <Route path="/" element={<div>Welcome to admin</div>} />
+        </Routes>
+      </div>
     </>
   );
 }
 function HomeNavbar() {
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
+    <Navbar expand="lg" bg="light" variant="light">
       <Container>
         <Navbar.Brand>
           <Nav.Link to="/" as={Link}>
-            Home
+            <img
+              src="alex-fxrwJGMCz_g-unsplash.jpeg"
+              alt="l logo"
+              width={100}
+              height={100}
+            />
           </Nav.Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link to="/blog/*" as={NavLink}>
+            <Nav.Link to="/blog/" as={NavLink}>
               Blog
             </Nav.Link>
             <Nav.Link to="/news" as={NavLink}>
